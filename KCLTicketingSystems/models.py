1f6520f1f936086a16afb278f894fb1cf829b572
k23163890 <k23163890@kcl.ac.uk>
2026-01-28 16:15:21 +0000

    Database

--- KCLTicketingSystems/models.py
+++ KCLTicketingSystems/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Ticket(models.Model):
+    name = models.CharField(max_length=255)
+    surname = models.CharField(max_length=255)
+    k_number = models.CharField(max_length=255, unique=True)
+    k_email = models.EmailField()
+    department = models.CharField(max_length=255)
+    issues = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.name} {self.surname}  - {self.k_number}"

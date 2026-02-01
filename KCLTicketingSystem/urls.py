02e67a68e64380d2c2dc32015e491ddbf035cbee
hafsabhudye <hafsa.bhudye@gmail.com>
2026-02-01 12:47:47 +0000

    refactor code into folders

--- KCLTicketingSystem/urls.py
+++ KCLTicketingSystem/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from KCLTicketingSystems import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('admin/', admin.site.urls),
-    path('', include('KCLTicketingSystems.urls')),
+    path('api/submit-ticket/', views.submit_ticket, name='submit_ticket'),
 ]
--- KCLTicketingSystems/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('api/submit-ticket/', views.submit_ticket, name='submit_ticket'),
-]
-

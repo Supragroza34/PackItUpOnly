c464d66ce0ce22a612c1e30198ce23c26a3edfeb
hafsabhudye <hafsa.bhudye@gmail.com>
2026-02-06 11:10:11 +0000

    edit reply view and template

--- KCLTicketingSystems/views/reply_view.py
+++ KCLTicketingSystems/views/reply_view.py
@@ -9,10 +9,7 @@ from django.urls import reverse
 
 def reply(request):         #ticket_id
     #current_user = request.user
-    ticket = Ticket.objects.get(pk=2)
-    current_reply_count = 0
-    if ticket.replies:
-        current_reply_count = ticket.replies.count
+    ticket = Ticket.objects.get(pk=1)
     if request.method == "POST":
         form = ReplyForm(request.POST, request.FILES)
         if form.is_valid():
@@ -32,7 +29,6 @@ def reply(request):         #ticket_id
     context = {
         'form': form,
         'ticket': ticket,
-        'current_reply_count': current_reply_count,
     }
 
     return render(request, 'reply.html', context)

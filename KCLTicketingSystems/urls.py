03b58f94f8ba35b6b98c50a4c51016e50c40a1a1
tejasraj15 <tejasraj905@gmail.com>
2026-02-10 00:23:01 +0000

    Remove unused app-level urls.py to avoid duplication

--- KCLTicketingSystems/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    # API endpoint for ticket submission
-    path('api/submit-ticket/', views.submit_ticket, name='submit_ticket'),
-]
-

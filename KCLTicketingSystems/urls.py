5c4ffbd1df1a2a7d5101671a0e5ea405dc5ab2bb
k23163890 <k23163890@kcl.ac.uk>
2026-02-23 12:57:32 +0000

    Merge conflict resolution

--- KCLTicketingSystems/urls.py
+++ KCLTicketingSystems/urls.py
@@ -4,6 +4,8 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views.auth import RegisterView
 from .views.users import MeView
 from .views.staff_dashboard_view import staff_dashboard
+from .views.reply_view import ReplyCreateView, ReplyView
+from .views.ticket_info_view import TicketDetailView
 
 urlpatterns = [
     path("auth/register/", RegisterView.as_view()),
@@ -11,4 +13,10 @@ urlpatterns = [
     path("auth/token/refresh/", TokenRefreshView.as_view()),
     path("users/me/", MeView.as_view()),
     path("staff-dashboard/", staff_dashboard, name="staff_dashboard"),
+    path("replies/create/", ReplyCreateView.as_view()),
+    path('reply/<int:ticket_id>/', ReplyView.as_view()),
+    path('tickets/<int:pk>', TicketDetailView.as_view()),
+    
 ]
+
+

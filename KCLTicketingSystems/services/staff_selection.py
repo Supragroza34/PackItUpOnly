74be6ac508033782d9f3bc967bc3c84c06aba9e1
hafsabhudye <hafsa.bhudye@gmail.com>
2026-03-08 12:36:10 +0000

    add staff selection tests

--- KCLTicketingSystems/services/staff_selection.py
+++ KCLTicketingSystems/services/staff_selection.py
@@ -1,29 +1,22 @@
 from ..models import User, Ticket
 
-def get_staff_with_least_tickets_in_department(department):
+def get_staff_id_with_least_tickets_in_department(department):
     staff = get_staff_from_department(department)
     staff_map = map_number_of_tickets_staff(staff)
     ticket_nums = staff_map.keys()
     smallest_num = min(ticket_nums)
-    return staff_map.get(smallest_num)
+    return staff_map.get(smallest_num)["id"]
 
-# def map_staff_to_number_of_tickets(staff):
-#     staff_map = {}
-#     for member in staff:
-#         number_of_tickets = get_number_of_tickets_assigned_to_staff(member.id)
-#         staff_map[member] = number_of_tickets
-#     return staff_map
-
-def map_number_of_tickets_staff(staff):
+def map_number_of_tickets_staff(staff_members):
     staff_map = {}
-    for member in staff:
-        number_of_tickets = get_number_of_tickets_assigned_to_staff(member.id)
+    for member in staff_members:
+        number_of_tickets = get_number_of_tickets_assigned_to_staff(member["id"])
         staff_map[number_of_tickets] = member
     return staff_map      
 
 def get_staff_from_department(department):
     staff_in_department = User.objects.filter(role__in=[User.Role.STAFF, User.Role.ADMIN], department=department).values(
-            'id', 'username', 'first_name', 'last_name', 'email', 'role'
+            'id', 'username', 'first_name', 'last_name', 'email', 'role', 'department',
         )
     return staff_in_department
 
